ab4bb3fe7edc7f8dfbbdc1abc931ed5cb728e19b
kengo6053 <kengohoi1027@gmail.com>
2025-04-12 15:15:08 +0900

    vscodeのファイルパスを変更

--- core.py
+++ core.py
@@ -1,40 +1,39 @@
-import webbrowser
-
+from subprocess import run
 import bpy
-
 from .register_class import _get_cls
 
 
-class COU_OT_open_text_file(bpy.types.Operator):
+class COT_OT_open_text_file(bpy.types.Operator):
     bl_idname = "object.open_text_file"
     bl_label = "Open Text File"
-    bl_description = "Open the URL of a text object."
+    bl_description = "Open the file of bpy.data.texts."
 
-    def execute(self, context):  # noqa: PLR6301
-        lst = list(bpy.data.objects)
-        if context.view_layer.objects.active:
-            lst = [context.view_layer.objects.active, *lst]
-        lst = [obj for obj in lst if obj.type == "FONT" and obj.data.body.startswith("http")]
-        if lst:
-            webbrowser.open(lst[0].data.body)
+    def execute(self, context):
+        # VS Code の実行ファイル（Code.exe）のフルパスを指定
+        code_exe_path = "C:/Users/hoike/AppData/Local/Programs/Microsoft VS Code/Code.exe"
+        for text in bpy.data.texts:
+            if s := text.filepath:
+                # ファイルパスを出力して変数の中身を確認
+                print("Opening file:", s)
+                run([code_exe_path, s])
         return {"FINISHED"}
-
+s
 
 # 自動的にこのモジュールのクラスを設定
 ui_classes = _get_cls(__name__)
 
 
-def draw_item(self, _context):
+def draw_item(self, context):
     """メニューの登録と削除用"""
     for ui_class in ui_classes:
         self.layout.operator(ui_class.bl_idname)
 
 
 def register():
-    """追加登録用(クラス登録は、register_class内で実行)"""
+    """追加登録用（クラス登録は、register_class内で実行）"""
     bpy.types.TEXT_MT_text.append(draw_item)
 
 
 def unregister():
-    """追加削除用(クラス削除は、register_class内で実行)"""
+    """追加削除用（クラス削除は、register_class内で実行）"""
     bpy.types.TEXT_MT_text.remove(draw_item)
